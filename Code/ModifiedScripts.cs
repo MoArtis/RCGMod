19111b181154fc8291da70b0bc3a48190c6a64e9
MoArtis <mathieuphilippe.dev@gmail.com>
2019-09-11 18:33:17 +0800

    Update files for the new 1.02 version

--- Code/ModifiedScripts.cs
+++ Code/ModifiedScripts.cs
@@ -1,62 +1,61 @@
 // ** PlayerInput.cs **
 protected override void Start()
 {
-	interactActionId = Mod.UserXpMod.interactActionId;
-	switch(interactActionId)
-	{
-		case "QuickAttack":
-			prevInteractCombatInputSet = x => { Quick = x; };
-			break;
-		case "HeavyAttack":
-			prevInteractCombatInputSet = x => { Heavy = x; };
-			break;
-		case "SpecialAttack":
-			prevInteractCombatInputSet = x => { Special = x; };
-			break;
-		case "Block":
-			prevInteractCombatInputSet = null;
-			break;
-		case "Recruit":
-			prevInteractCombatInputSet = x => { Recruit = x; };
-			break;
-		case "Jump":
-			prevInteractCombatInputSet = x => { Jump = x; };
-			break;
-	}
-	
-	//Truncated code
-}
+    interactActionId = Mod.UserXpMod.interactActionId;
+    switch (interactActionId)
+    {
+        case "QuickAttack":
+            prevInteractCombatInputSet = x => { Quick = x; };
+            break;
+        case "HeavyAttack":
+            prevInteractCombatInputSet = x => { Heavy = x; };
+            break;
+        case "SpecialAttack":
+            prevInteractCombatInputSet = x => { Special = x; };
+            break;
+        case "Block":
+            prevInteractCombatInputSet = null;
+            break;
+        case "Recruit":
+            prevInteractCombatInputSet = x => { Recruit = x; };
+            break;
+        case "Jump":
+            prevInteractCombatInputSet = x => { Jump = x; };
+            break;
+    }
 
+    //Truncated code
+}
 
 private void Update()
 {
-	//Added local function
-	void HandleInteractAction(System.Action<bool> combatInputSet)
-	{
-		if(!InInteractRange)
-		{
-			base.InteractQuick = false;
-		}
-		else
-		{
-			combatInputSet(false);
-		}
-	}
-	
-	//Lines 193 to 202
-	//if (!InInteractRange)
-	//{
-	base.Quick = _rewiredPlayer.GetButtonDown("QuickAttack");
-	//base.InteractQuick = false;
-	//}
-	//else
-	//{
-	//base.Quick = false;
-	base.InteractQuick = _rewiredPlayer.GetButtonDown(interactActionId);
-	//}
-	
-	//Line 278
-	HandleInteractAction(prevInteractCombatInputSet);
+    //Added local function
+    void HandleInteractAction(System.Action<bool> combatInputSet)
+    {
+        if (!InInteractRange)
+        {
+            base.InteractQuick = false;
+        }
+        else
+        {
+            combatInputSet(false);
+        }
+    }
+
+    //Lines 193 to 202
+    //if (!InInteractRange)
+    //{
+    base.Quick = _rewiredPlayer.GetButtonDown("QuickAttack");
+    //base.InteractQuick = false;
+    //}
+    //else
+    //{
+    //base.Quick = false;
+    base.InteractQuick = _rewiredPlayer.GetButtonDown(interactActionId);
+    //}
+
+    //Line 278
+    HandleInteractAction(prevInteractCombatInputSet);
 }
 
 // ** UI_ScreenIntro.cs **
@@ -99,8 +98,23 @@ public void OnItemChange(bool _skipResetMusic = false)
     //Truncated code
 }
 
-// ** PlayerManager.cs - Start Method - At the end of the method
+// ** PlayerManager.cs ** - Start Method - At the end of the method
 private void Start()
 {
     Mod.UserXpMod.LoadCustomInputConfig();
+}
+
+// ** PlayerUseableEffectManager.cs **
+public bool IsInEffectHeld(int inPlayerID, UseableHeldEffect inEffect)
+{
+    int id = Mod.UserXpMod.fixBooksBug ? (int)PlayerManager.Instance.Players[inPlayerID].ClassNameToPlayerCharacter : inPlayerID;
+    return PlayerGlobalInventory.instance.PlayerInventories[id].UseablesInventory.Items.Exists((Data_Item o) => o is Data_InventoryItem && (o as Data_InventoryItem).HeldEffect == inEffect);
+}
+
+// ** UI_PhoneScreen_CharacterV2.cs **
+protected void SetStats()
+{
+    //Add this just after the line 135
+    if (_characterStats[i].StatToWatch == PlayerAttributeEnum.Stamina && Mod.UserXpMod.fixMaxStaminaBug)
+        maxStatLevel = 39;
 }
